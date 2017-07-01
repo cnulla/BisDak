1f6e0966ecfa46afbc0cfbf367a1e2baf24762f1
Eleanor Kate <Eleanor Kate>
2017-07-01 09:42:30 +0800

    First commit

---
+++ resources/views/auth/register.blade.php
@@ -0,0 +1,169 @@
+@extends('layouts.app')
+
+@section('content')
+ <!-- External CSS -->
+<link href="./css/bootstrap.min.css" rel="stylesheet">
+<link href="./css/app.css" rel="stylesheet">
+<link href="{{ asset('css/developer.css') }}" rel="stylesheet">
+<link href="./css/register.css" rel="stylesheet">
+<!-- Fonts -->
+<link href="./fonts/raleway.css" rel="stylesheet" type="text/css">
+
+<div class="container flex-center position-ref ">
+    <div class="row">
+            <h1 class="text-center login-title">
+                <strong>
+                REGISTER AS A DEVELOPER
+                </strong>
+            </h1>
+            <div class="account-wall">
+                <!--Logo-->
+                <img class="profile-img" src="https://lh5.googleusercontent.com/-b0-k99FZlyE/AAAAAAAAAAI/AAAAAAAAAAA/eu7opA4byxI/photo.jpg?sz=120"
+                    alt="">
+               
+                <!--Form-->
+                <form class="form-horizontal" role="form" method="POST" action="{{ route('register') }}">
+                    {{ csrf_field() }}
+
+                    <div class="form-group{{ $errors->has('name') ? ' has-error' : '' }}">
+                        <label for="name" class="col-md-4 control-label">Name</label>
+
+                        <div class="col-md-6">
+                            <input id="name" type="text" class="form-control" name="name" value="{{ old('name') }}" required autofocus>
+
+                            @if ($errors->has('name'))
+                                <span class="help-block">
+                                    <strong>{{ $errors->first('name') }}</strong>
+                                </span>
+                            @endif
+                        </div>
+                    </div>
+
+                    <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
+                        <label for="email" class="col-md-4 control-label">E-Mail Address</label>
+
+                        <div class="col-md-6">
+                            <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required>
+
+                            @if ($errors->has('email'))
+                                <span class="help-block">
+                                    <strong>{{ $errors->first('email') }}</strong>
+                                </span>
+                            @endif
+                        </div>
+                    </div>
+
+                    <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
+                        <label for="password" class="col-md-4 control-label">Password</label>
+
+                        <div class="col-md-6">
+                            <input id="password" type="password" class="form-control" name="password" required>
+
+                            @if ($errors->has('password'))
+                                <span class="help-block">
+                                    <strong>{{ $errors->first('password') }}</strong>
+                                </span>
+                            @endif
+                        </div>
+                    </div>
+
+                    <div class="form-group">
+                        <label for="password-confirm" class="col-md-4 control-label">Confirm Password</label>
+
+                        <div class="col-md-6">
+                            <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required>
+                        </div>
+                    </div>
+
+                    <div class="form-group">
+                        <!--Button-->
+                        <div class="col-md-3 col-md-offset-7">
+                            <button type="submit" class="btn btn-lg btn-primary btn-block btn-hd">
+                                REGISTER
+                            </button>
+                            <span class="clearfix"></span>
+                        </div>
+                    </div>
+                </form>
+            </div>
+
+            <!--Create Account-->
+            <a href="{{ url('/login') }}" class="text-center new-account"><strong>Already have an account?</strong></a>
+        
+    </div>
+</div>
+
+
+<!--<div class="container">
+    <div class="row">
+        <div class="col-md-8 col-md-offset-2">
+            <div class="panel panel-default">
+                <div class="panel-heading">Register</div>
+                <div class="panel-body">
+                    <form class="form-horizontal" role="form" method="POST" action="{{ route('register') }}">
+                        {{ csrf_field() }}
+
+                        <div class="form-group{{ $errors->has('name') ? ' has-error' : '' }}">
+                            <label for="name" class="col-md-4 control-label">Name</label>
+
+                            <div class="col-md-6">
+                                <input id="name" type="text" class="form-control" name="name" value="{{ old('name') }}" required autofocus>
+
+                                @if ($errors->has('name'))
+                                    <span class="help-block">
+                                        <strong>{{ $errors->first('name') }}</strong>
+                                    </span>
+                                @endif
+                            </div>
+                        </div>
+
+                        <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
+                            <label for="email" class="col-md-4 control-label">E-Mail Address</label>
+
+                            <div class="col-md-6">
+                                <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required>
+
+                                @if ($errors->has('email'))
+                                    <span class="help-block">
+                                        <strong>{{ $errors->first('email') }}</strong>
+                                    </span>
+                                @endif
+                            </div>
+                        </div>
+
+                        <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
+                            <label for="password" class="col-md-4 control-label">Password</label>
+
+                            <div class="col-md-6">
+                                <input id="password" type="password" class="form-control" name="password" required>
+
+                                @if ($errors->has('password'))
+                                    <span class="help-block">
+                                        <strong>{{ $errors->first('password') }}</strong>
+                                    </span>
+                                @endif
+                            </div>
+                        </div>
+
+                        <div class="form-group">
+                            <label for="password-confirm" class="col-md-4 control-label">Confirm Password</label>
+
+                            <div class="col-md-6">
+                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required>
+                            </div>
+                        </div>
+
+                        <div class="form-group">
+                            <div class="col-md-6 col-md-offset-4">
+                                <button type="submit" class="btn btn-primary">
+                                    Register
+                                </button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>-->
+@endsection
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,82 @@
+<!doctype html>
+<html lang="{{ app()->getLocale() }}">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>BisDak</title>
+
+        <!-- External CSS -->
+       <link href="./css/bootstrap.min.css" rel="stylesheet">
+       <link href="./css/welcome.css" rel="stylesheet">
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+
+    </head>
+    <body>
+
+        <div class="flex-center position-ref full-height">
+            <div class="top-right links"> 
+            @if (Route::has('login'))
+                @if (Auth::check())
+                    <a href="{{ url('/home') }}">Home</a>
+                @else
+                    <a href="{{ url('/developer') }}">Developers</a>
+                @endif
+            @endif
+            </div>
+
+            <div class="content">
+                <div class="title header">
+                   BisDak
+                </div>
+                <div class="subtitle subheader">
+                    A Rule-based Morphological Analyzer for Cebuano Language
+                </div>
+                <hr class="md">
+                <div class="container">
+                    <div class="tag-box">
+                        <form>
+                            <div class="form-group">
+                                <input type="text" class="form-control" id="input_word" name="wordInput" placeholder="Input a word...
+                                ">
+                            </div>
+                            <div class="form-group"> 
+                                <button type="submit" class="btn btn-hd">ANALYZE</button>
+                            </div>                      
+                        </form> 
+                    </div>
+                </div>
+                <hr class="md">
+                <div class="container info ">
+                    <h1 class="subtitle"><strong>Output</strong></h1>
+                    <hr class="nd">
+                    
+                    <div class="baseform">
+                        <h4><strong>BASE FORM</strong></h4>
+                        <h5><center>Root Word</center></h5>
+                        <hr class="nd">
+
+                        <h4><strong>AFFIXES</strong></h4>
+                        <h6>Prefix | </h6>
+                        <h6>Infix | </h6>
+                        <h6>Suffix | </h6>
+
+                    </div>
+                    <div class="sentence">
+                        <h4><strong>AMBIGUITY</strong></h4>
+                        <hr class="nd">
+                        <h5><strong>Part of Speech</strong></h5>
+                        <h6>Example Sentence</h6>
+                    </div>
+                
+                </div>
+            </div>
+        </div>
+
+        <script src="./js/jquery.min.js"></script>
+        <script src="./js/bootstrap.min.js"></script>
+    </body>
+</html>
